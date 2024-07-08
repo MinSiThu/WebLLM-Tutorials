b236af34c60fa0586eb3e129af317b012a442485
Min Si Thu <archimedes557@gmail.com>
2024-07-08 10:41:37 +0700

    feat: WebGPU

---
+++ webgpu-check/app.js
@@ -0,0 +1,15 @@
+async function init() {
+    if (!navigator.gpu) {
+      throw Error("WebGPU not supported.");
+    }
+  
+    const adapter = await navigator.gpu.requestAdapter();
+    if (!adapter) {
+      throw Error("Couldn't request WebGPU adapter.");
+    }
+  
+    const device = await adapter.requestDevice();
+    console.log(device);
+}
+
+init();
